33d76a4f54ec899a03d4c98c62e968c3923071ba
ulyana <bsergeybabak2000@gmail.com>
2023-04-04 23:13:06 +0300

    второй комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static boolean isYearLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
+
+    public static void checkOS(int os, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (os == 0) {
+            if (currentYear > year) {
+                System.out.println("Нужна облегчённая версия для  IOS");
+            } else {
+                System.out.println("Установитя обычную версию для IOS");
+            }
+        } else if (os == 1) {
+            if (currentYear > year) {
+                System.out.println("Нужна облегчённая версия для  Android");
+            } else {
+                System.out.println("Установитя обычную версию для Android");
+            }
+        } else {
+            System.out.println("Система не поддерживается");
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2021;
+        if (isYearLeap(year)) {
+            System.out.println("Год являетсяя високосным");
+        } else {
+            System.out.println("Год не является високосным");
+        }
+
+
+    }
+
 }
+
+
